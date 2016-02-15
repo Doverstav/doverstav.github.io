c7402422a8ee6d1286edbfcd43a85cf90dc3592d
Pontus Doverstav <doverstav@hotmail.com>
2016-02-15 23:53:29 +0100

    Implemented a funciotn for calculating logistic growth

--- scripts/mapUtils.js
+++ scripts/mapUtils.js
@@ -131,3 +131,11 @@ function checkAdjacentTiles(x, y, scopeX, scopeY, instance, mapToUse){
 
     return instanceCounter;
 }
+
+// Returns the change in population
+// r = Growth rate
+// N = Current population
+// K = Max allowed population
+function logGrowth (r,N,K) {
+    return ((r*N) * (1 - (N/K)));
+}
