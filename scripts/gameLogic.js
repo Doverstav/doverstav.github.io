a9faaf0ccf4c01ec18ff5158c127334099ccaa06
Pontus Doverstav <doverstav@hotmail.com>
2016-02-16 12:23:09 +0100

    Added gameLoop and implemented updateVillagePopulations

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -9,6 +9,20 @@ function getVillageInstances (mapToUse) {
     villageInstances = findAllInstances(mapToUse,village);
 }
 
+function gameLoop (mapToUse) {
+    updateVillagePopulations();
+}
+
+function updateVillagePopulations () {
+    for(var i = 0; i < villageInstances.length; i++){
+        var curPop = villageInstances[i].population;
+        var gR = villageInstances[i].growthRate;
+        var maxPop = villageInstances[i].maxPop;
+        var deltaPop = logGrowth(gR, curPop, maxPop);
+        villageInstances[i].setPopulation((curPop + deltaPop));
+    }
+}
+
 function logGrowth (r,N,K) {
     return ((r*N) * (1 - (N/K)));
 }
