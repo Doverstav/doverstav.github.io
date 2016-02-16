2d52f37bbc421cbc97864dab8572023c290f081a
Pontus Doverstav <doverstav@hotmail.com>
2016-02-16 12:01:51 +0100

    Added function and variable for finding and saving all village instances

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -2,6 +2,13 @@
 // r = Growth rate
 // N = Current population
 // K = Max allowed population
+
+var villageInstances = [];
+
+function getVillageInstances (mapToUse) {
+    villageInstances = findAllInstances(mapToUse,village);
+}
+
 function logGrowth (r,N,K) {
     return ((r*N) * (1 - (N/K)));
 }
