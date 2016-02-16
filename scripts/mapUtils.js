5a1deb834ba0cb52705c5753c0177b7d730f93ab
Pontus Doverstav <doverstav@hotmail.com>
2016-02-16 11:13:30 +0100

    Removed logGrowth

--- scripts/mapUtils.js
+++ scripts/mapUtils.js
@@ -131,11 +131,3 @@ function checkAdjacentTiles(x, y, scopeX, scopeY, instance, mapToUse){
 
     return instanceCounter;
 }
-
-// Returns the change in population
-// r = Growth rate
-// N = Current population
-// K = Max allowed population
-function logGrowth (r,N,K) {
-    return ((r*N) * (1 - (N/K)));
-}
